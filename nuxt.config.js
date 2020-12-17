dc09f585273c4899c240ffa27658c5ac596d019a
ojaros <oliverjaros214@gmail.com>
2020-12-16 22:21:43 -0700

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,10 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'server',
+  target: 'static',
+  router: {
+    base: '/ojaros.github.io/'
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
